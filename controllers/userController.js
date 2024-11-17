6c25762dad9167ec6cb61c6d967d571775190092
PucTv <128126363+ThaiVanPhuc@users.noreply.github.com>
2024-11-17 18:50:19 +0700

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,3 +10,25 @@ exports.getAllUsers = async (req, res) => {
     res.status(500).json({ message: "Error fetching users" });
   }
 };
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email, password });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    res.json({
+      message: "Login successful",
+      user: {
+        id: user._id,
+        full_name: user.full_name,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    console.log("Error logging in:", err);
+    res.status(500).json({ message: "Error logging in" });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.get("/", userController.getAllUsers);
+router.post("/login", userController.loginUser);
 module.exports = router;
