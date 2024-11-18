8857dcbe7fdd62053a0d23270f2d6ce184c99cbb
PucTv <128126363+ThaiVanPhuc@users.noreply.github.com>
2024-11-18 21:12:12 +0700

    update

--- config/config.js
+++ config/config.js
@@ -1,16 +1,32 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+const Trip = require("../models/tripModel");
 
 const connectDB = async () => {
-  try {
-    // Ket noi voi mongodb
-    await mongoose.connect(
-      "mongodb+srv://phuc100662:w5wVbrX6eaMKBsQu@cluster0.kkdpq.mongodb.net/flutter_db?retryWrites=true&w=majority&appName=Cluster0"
-    );
-    console.log("Connect successfully!!!");
-  } catch (error) {
-    console.log("Connect failure!!!", error);
+  const mongoUri = process.env.MONGO_URI;
+  if (!mongoUri) {
+    console.error("Lỗi: MONGO_URI không được định nghĩa trong file .env");
     process.exit(1);
   }
+  mongoose
+    .connect(mongoUri)
+    .then(() => {
+      console.log("Đã kết nối thành công tới MongoDB Atlas");
+      processDatabase();
+    })
+    .catch((err) => {
+      console.error("Lỗi kết nối:", err);
+      process.exit(1);
+    });
 };
-module.exports = connectDB;
+
+const processDatabase = async () => {
+  try {
+    const trips = await Trip.find();
+    console.log("Kết nối thành công");
+  } catch (err) {
+    console.error("Lỗi khi xử lý dữ liệu:", err);
+  }
+};
+
+module.exports = { connectDB };
