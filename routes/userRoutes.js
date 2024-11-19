ae65e1a716b898aa3802d4b0a4c6d41420dea0ad
PucTv <128126363+ThaiVanPhuc@users.noreply.github.com>
2024-11-19 15:21:13 +0700

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,4 +4,14 @@ const userController = require("../controllers/userController");
 
 router.get("/", userController.getAllUsers);
 router.post("/login", userController.loginUser);
+
+// Thêm người dùng mới
+router.post("/create", userController.createUser);
+
+// Sửa thông tin người dùng
+router.put("/update/:id", userController.updateUser);
+
+// Xóa người dùng
+router.delete("/delete/:id", userController.deleteUser);
+
 module.exports = router;
